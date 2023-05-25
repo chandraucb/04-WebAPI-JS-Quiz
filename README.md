b51a3857a21c4300b2e9c43eccf1dc92c830837d
Chandrasekar Mohan <chandrasekar.mohan@gmail.com>
2023-05-25 02:20:42 -0700

    Added result section content

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 04-WebAPI-JS-Quiz
+
+## Reference
+JS questions and answers are referred from below link,
+https://www.interviewbit.com/javascript-mcq/
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,71 +2,103 @@ var viewHighScoreNav = document.getElementById('high-score')
 var highScoreSection = document.getElementById('high-score-section')
 var introScoreSection = document.getElementById('intro-section')
 var quizSection = document.getElementById('quiz-section')
+var resultSection= document.getElementById('result-section')
 var startQuiz = document.getElementById('startQuiz')
 var goBack = document.getElementById ('goBack')
 var quizArticle = document.getElementById('quizArticle')
+var timerCount = document.getElementById('timerCount')
+
+var currentScore = 0
+var remainingTime = 30
+var remainQuestion = 5
+
+//Array with list of questions with its choices and answer
+const questions = [
+    {
+        question : 'Which object in Javascript doesn’t have a prototype?',
+        choices : ['Base Object','All objects have prototype','None of the objects have prototype','None of the above'],
+        answer : 'Base Object'
+    },
+    {
+        question : 'Which of the following are not server-side Javascript objects',
+        choices : ['Date','FileUpload','Function','All of the above'],
+        answer : 'All of the above'
+    },
+    {
+        question : 'How do we write a comment in javascript?',
+        choices : ['<!-- -->','//','#','$$'],
+        answer : '//'
+    },
+    {
+        question : 'How are objects compared when they are checked with the strict equality operator?',
+        choices : ['The contents of the objects are compared','Their references are compared','Both A and B','None of the above'],
+        answer : 'Their references are compared'
+    },
+    {
+        question : 'What does … operator do in JS?',
+        choices : ['It is used to spread iterables to individual elements','It is used to define a datatype of undefined size','No such operator exists','None of the above'],
+        answer : 'It is used to spread iterables to individual elements'
+    },
+    {
+        question : 'How to stop an interval timer in Javascript?',
+        choices : ['clearInterval','clearTimer','intervalOver','None of the above'],
+        answer : 'clearInterval'
+    },
+    {
+        question : 'Which of the following are closures in Javascript?',
+        choices : ['Variables','Functions','Objects','All of the above'],
+        answer : 'All of the above'
+    },
+    {
+        question : 'Which function is used to serialize an object into a JSON string in Javascript?',
+        choices : ['parse()','stringify()','convert()','None of the above'],
+        answer : 'stringify()'
+    },
+    {
+        question : 'What is the output of print(NaN === NaN); ?',
+        choices : ['true','false','undefined','Error'],
+        answer : 'false'
+    },
+    {
+        question : 'What does the Javascript “debugger” statement do?',
+        choices : ['It will debug all the errors in the program at runtime','It acts as a breakpoint in a program','It will debug error in the current statement if any','All of the above'],
+        answer : 'It acts as a breakpoint in a program'
+    }
+]
+
+var usedQuestionIndex = []
+
+function loadResultSection () {
+
+    var finalScore = document.getElementById('finalScore')
+    quizSection.style.display = 'none'
+    resultSection.style.display = 'block'
+    finalScore.textNode="Your final score is " + currentScore
+}
 
 // call createUpdateQuizQuestion with next question
 function loadNextQuestion () {
-    //Array with list of questions with its choices and answer
-    var questions = [
-        {
-            question : 'Which object in Javascript doesn’t have a prototype?',
-            choices : ['Base Object','All objects have prototype','None of the objects have prototype','None of the above'],
-            answer : 'Base Object'
-        },
-        {
-            question : 'Which of the following are not server-side Javascript objects',
-            choices : ['Date','FileUpload','Function','All of the above'],
-            answer : 'All of the above'
-        },
-        {
-            question : 'How do we write a comment in javascript?',
-            choices : ['<!-- -->','//','#','$$'],
-            answer : '//'
-        },
-        {
-            question : 'How are objects compared when they are checked with the strict equality operator?',
-            choices : ['The contents of the objects are compared','Their references are compared','Both A and B','None of the above'],
-            answer : 'Their references are compared'
-        },
-        {
-            question : 'What does … operator do in JS?',
-            choices : ['It is used to spread iterables to individual elements','It is used to define a datatype of undefined size','No such operator exists','None of the above'],
-            answer : 'It is used to spread iterables to individual elements'
-        },
-        {
-            question : 'How to stop an interval timer in Javascript?',
-            choices : ['clearInterval','clearTimer','intervalOver','None of the above'],
-            answer : 'clearInterval'
-        },
-        {
-            question : 'Which of the following are closures in Javascript?',
-            choices : ['Variables','Functions','Objects','All of the above'],
-            answer : 'All of the above'
-        },
-        {
-            question : 'Which function is used to serialize an object into a JSON string in Javascript?',
-            choices : ['parse()','stringify()','convert()','None of the above'],
-            answer : 'stringify()'
-        },
-        {
-            question : 'What is the output of print(NaN === NaN); ?',
-            choices : ['true','false','undefined','Error'],
-            answer : 'false'
-        },
-        {
-            question : 'What does the Javascript “debugger” statement do?',
-            choices : ['It will debug all the errors in the program at runtime','It acts as a breakpoint in a program','It will debug error in the current statement if any','All of the above'],
-            answer : 'It acts as a breakpoint in a program'
-        }
-    ]
+
+    //No questions or if all questions used just return
+    if (questions.length <= 0 || usedQuestionIndex.length === questions.length )
+        return
 
     //random index selector
     var randNum = Math.floor(Math.random() * questions.length);
 
+    //Remove selected question to avoid repeats
+    while (usedQuestionIndex.includes(randNum) ) {
+        randNum = Math.floor(Math.random() * questions.length);
+    }
+    usedQuestionIndex.push(randNum)
+   
+    var question = questions[randNum]
+
+    
+    //questions.splice(randNum,1)
+
     // create question article with selected index question object 
-    createUpdateQuizQuestion ( questions[randNum].question, questions[randNum].choices, questions[randNum].answer )
+    createUpdateQuizQuestion ( question.question, question.choices, question.answer )
 }
 
 // display result of last answered question
@@ -93,15 +125,28 @@ function createUpdateQuizQuestion (question, choices, answer) {
 
     // function to act on user picked answer, 
     var selectAnswer = function (event) {
-        console.log(event.target)
+        remainQuestion-=1
         var result = "Wrong!"
         if (event.target.textContent === answer) {
+            //increment current score if correct
+            currentScore=+2
             result = "Correct!"
+        } else {
+            //penalize for incorrect answer 
+            currentScore-=1
+            remainingTime-=10
         }
-
-        loadNextQuestion()
-
-        showResult(result, quizArticle)
+        //check if max questions or time has exceeded
+        if (remainQuestion > 0 && remainingTime > 0) {
+            //load next question from the list
+            loadNextQuestion()
+            showResult(result, quizArticle)
+        } else {
+            loadResultSection()
+            showResult(result, resultSection)
+        }
+        
+        
     }
 
     //OL element to show answer choices
@@ -134,13 +179,27 @@ function createUpdateQuizQuestion (question, choices, answer) {
 startQuiz.addEventListener('click',function(event){
     introScoreSection.style.display = 'none'
     quizSection.style.display = 'block'
+    
     loadNextQuestion () // loads next question be to answered
+
+    timerCount.textContent = remainingTime;
+
+    var intervalRef = setInterval (function () {
+
+        remainingTime--;
+
+        timerCount.textContent = remainingTime;
+
+        if (remainingTime <= 0) {
+            clearInterval(intervalRef)
+        }
+
+    }, 1000)
 })
 
 //View High Score section Go Back event listener 
 goBack.addEventListener('click', function(event){
-    introScoreSection.style.display = 'block'
-    highScoreSection.style.display = 'none'
+    location.reload();
 })
 
 //View High Score link navigation event listener 
@@ -148,6 +207,8 @@ viewHighScoreNav.addEventListener('click',function(event){
     introScoreSection.style.display = 'none'
     quizSection.style.display = 'none'
     highScoreSection.style.display = 'block'
+    //setInterval will decrement and clears the timer based on remainingTime
+    remainingTime = 1 
 })
 
 
